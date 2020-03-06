c7311f1348e367a31d335246360a53e10111e644
Daniel <daniel.jablonski1998@gmail.com>
2020-03-06 20:26:10 +0100

    Dodaj pliki projektów.

---
+++ RPQ.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#define FILEPATH "C:\\Users\\Student241165\\Desktop\\RPQ\\data10.txt"
+
+class RPQ {
+public:
+    RPQ() {
+        loadFile();
+    }
+private:
+    std::vector<std::vector<int>> VectorOfProcesses;
+    std::vector<int> Process;
+    int numberOfProcess = 0;
+    int numberOfParameters = 0;
+    int buffer;
+
+public:
+    void loadFile() {
+        std::ifstream inputFile;
+        inputFile.open(FILEPATH);
+        if (inputFile.is_open()) {
+            inputFile >> numberOfProcess;
+            inputFile >> numberOfParameters;
+           for (int j = 0; j < numberOfProcess; j++) {
+                Process.clear();
+                for (int i = 0; i < numberOfParameters; i++) {
+                    inputFile >> buffer;
+                    Process.push_back(buffer);
+                }
+                VectorOfProcesses.push_back(Process);
+            }
+        }
+        else {
+            std::cout << "Nie ma takiego pliku";
+        }
+        inputFile.close();
+    }
+    void PrintProcesses() {
+        for (auto i : VectorOfProcesses) {
+            for (auto j : i) {
+                std::cout << j << ' ';
+            }
+            std::cout << std::endl;
+        }
+    }
+    
+};
+
+int main() {
+    RPQ rpq;
+    rpq.PrintProcesses();
+    return 0;
+}
