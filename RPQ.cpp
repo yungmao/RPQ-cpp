fefbc14371727a17baf2105c784e28de273ac99d
Daniel <daniel.jablonski1998@gmail.com>
2020-03-06 23:10:20 +0100

    Begining of implementing Carlier, Schrange, RQ

--- RPQ.cpp
+++ RPQ.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <chrono>
 
-#define FILEPATH "C:\\Users\\Student241165\\Desktop\\RPQ\\data100.txt"
+#define FILEPATH "C:\\Users\\Student241165\\Desktop\\RPQ\\data50.txt"
 
 class RPQ {
 public:
@@ -24,17 +24,22 @@ private:
 public:
     void loadFile();
     void PrintProcesses();
-    void SortByR();
-    void SortByPermutation();
     void CMAX();
+    void SortByR();
+    void SortByQR();
+    void SortCarlier();
+    void SortSchrange();
+    
 };
 
+//Main Function
 int main() {
     RPQ rpq;
     rpq.PrintProcesses();
     return 0;
 }
 
+// Definig methods
 void RPQ::loadFile() {
     std::ifstream inputFile;
     inputFile.open(FILEPATH);
@@ -69,9 +74,8 @@ void RPQ::SortByR() {
     std::sort(VectorOfProcesses.begin(), VectorOfProcesses.end());
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    std::cout << "Czas Wykonania funkcji sort R: " << duration <<"ms"<<std::endl;
+    std::cout << "Czas Wykonania funkcji sort R: " << duration << "ms" << std::endl;
 }
-void RPQ::SortByPermutation() {}
 void RPQ::CMAX() {
     int endtime = 0;
     int cmax = 0;
@@ -99,4 +103,60 @@ void RPQ::CMAX() {
     }
 
     std::cout << "Wynik CMAX = " << endtime << std::endl;
+}
+void RPQ::SortByQR() {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::sort(VectorOfProcesses.begin(), VectorOfProcesses.end(),QR);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    std::cout << "Czas Wykonania funkcji sort RQ: " << duration << "ms" << std::endl;
+}
+void RPQ::SortCarlier(){
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::sort(VectorOfProcesses.begin(), VectorOfProcesses.end(),Carlier);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    std::cout << "Czas Wykonanie algorytmy Carliera: " << duration << "ms" << std::endl;
+}
+void RPQ::SortSchrange() {
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::sort(VectorOfProcesses.begin(), VectorOfProcesses.end(),Schrange);
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    std::cout << "Czas Wykonanie Algorytmu Schrange " << duration << "ms" << std::endl;
+
+}
+
+bool Schrange(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    for (size_t i = 0; i < vec1.size() && i < vec2.size(); i++) {
+        if (vec1[i] > vec2[i]) {
+            return false;
+        }
+        else if (vec1[i] < vec2[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+bool Carlier(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    for (size_t i = 0; i < vec1.size() && i < vec2.size(); i++) {
+        if (vec1[i] > vec2[i]) {
+            return false;
+        }
+        else if (vec1[i] < vec2[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+bool QR(const std::vector<int>& vec1, const std::vector<int>& vec2) {
+    for (size_t i = 0; i < vec1.size() && i < vec2.size(); i++) {
+        if (vec1[i] > vec2[i]) {
+            return false;
+        }
+        else if (vec1[i] < vec2[i]) {
+            return true;
+        }
+    }
+    return false;
 }
