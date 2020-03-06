5bebd83acf52041baf60f9925c177c919316111f
Daniel <daniel.jablonski1998@gmail.com>
2020-03-06 20:31:22 +0100

    refaktoryzacja

--- RPQ.cpp
+++ RPQ.cpp
@@ -18,39 +18,42 @@ private:
     int buffer;
 
 public:
-    void loadFile() {
-        std::ifstream inputFile;
-        inputFile.open(FILEPATH);
-        if (inputFile.is_open()) {
-            inputFile >> numberOfProcess;
-            inputFile >> numberOfParameters;
-           for (int j = 0; j < numberOfProcess; j++) {
-                Process.clear();
-                for (int i = 0; i < numberOfParameters; i++) {
-                    inputFile >> buffer;
-                    Process.push_back(buffer);
-                }
-                VectorOfProcesses.push_back(Process);
-            }
-        }
-        else {
-            std::cout << "Nie ma takiego pliku";
-        }
-        inputFile.close();
-    }
-    void PrintProcesses() {
-        for (auto i : VectorOfProcesses) {
-            for (auto j : i) {
-                std::cout << j << ' ';
-            }
-            std::cout << std::endl;
-        }
-    }
-    
+    void loadFile();
+    void PrintProcesses();
+
 };
 
 int main() {
     RPQ rpq;
     rpq.PrintProcesses();
     return 0;
+}
+
+void RPQ::loadFile() {
+    std::ifstream inputFile;
+    inputFile.open(FILEPATH);
+    if (inputFile.is_open()) {
+        inputFile >> numberOfProcess;
+        inputFile >> numberOfParameters;
+        for (int j = 0; j < numberOfProcess; j++) {
+            Process.clear();
+            for (int i = 0; i < numberOfParameters; i++) {
+                inputFile >> buffer;
+                Process.push_back(buffer);
+            }
+            VectorOfProcesses.push_back(Process);
+        }
+    }
+    else {
+        std::cout << "Nie ma takiego pliku";
+    }
+    inputFile.close();
+}
+void RPQ::PrintProcesses() {
+    for (auto i : VectorOfProcesses) {
+        for (auto j : i) {
+            std::cout << j << ' ';
+        }
+        std::cout << std::endl;
+    }
 }
